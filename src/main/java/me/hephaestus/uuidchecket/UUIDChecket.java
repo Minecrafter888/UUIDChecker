8e341ee72a74558e7dbe95e4e9a6fdd9228c2553
minecrafter888 <kyandro2004@gmail.com>
2023-05-26 11:07:20 +0200

    added educational features to UUIDChecket

--- src/main/java/me/hephaestus/uuidchecket/UUIDChecket.java
+++ src/main/java/me/hephaestus/uuidchecket/UUIDChecket.java
@@ -7,6 +7,8 @@ public final class UUIDChecket extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        //in the getCommand() you need to add what you want the command to be in ""
+        //instead of new UUIDCheckerCommand you need to call forward the name of your command class
         getCommand("uuid").setExecutor(new UUIDCheckerCommand());
 
     }
