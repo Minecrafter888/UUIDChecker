abf9d9ca3c2c7588ca47fa2291e6594a78038298
Minecrafter888 <118668882+Minecrafter888@users.noreply.github.com>
2023-06-07 14:51:04 +0200

    Update UUIDChecker.java

--- src/main/java/me/hephaestus/uuidchecker/UUIDChecker.java
+++ src/main/java/me/hephaestus/uuidchecker/UUIDChecker.java
@@ -12,8 +12,11 @@ public final class UUIDChecker extends JavaPlugin {
         //in the getCommand() you need to add what you want the command to be in ""
         //instead of new UUIDCheckerCommand you need to call forward the name of your command class
         getCommand("uuid").setExecutor(new UUIDCheckerCommand());
+        //the following code is how to make it so luckperms registers the permission
         PluginManager pluginManager = getServer().getPluginManager();
+        //the new permission is the name of the permission
         Permission yourPermission = new Permission("UUIDChecker.uuid");
+        //where you add the permission
         pluginManager.addPermission(yourPermission);
 
 
